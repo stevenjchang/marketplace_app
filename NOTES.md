ee52052c3dc9b7bbaa65b394e5b73ba1c18e42f3
stevenjchang <stevenjchang@gmail.com>
2019-05-18 12:24:46 -0700

    add AddListing component

--- NOTES.md
+++ NOTES.md
@@ -4,3 +4,6 @@ TODO:
 2. [x] handleSubmit to save to store
 3. [x] make constants file
 4. [x] add search filter, v1 - with multiple .filters
+5. [ ] ability to add listing, mocked
+6. [ ] redirect view to list after adding
+7. [ ] 
---
+++ client/src/components/AddListing.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class AddListing extends Component {
+  state = {}
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = () => {
+    axios.post('/listings/add', this.state)
+    // send this.state to server
+    // this.props.history.push(`/posts`)
+  }
+
+  render() {
+    return (
+      <>
+        <input
+          name="title"
+          type="text"
+          placeholder="Title"
+          onChange={this.handleChange}
+        />
+        <input
+          name="price"
+          type="text"
+          placeholder="Minimum Price"
+          onChange={this.handleChange}
+        />
+        <input
+          name="bedrooms"
+          type="text"
+          placeholder="# of bedrooms"
+          onChange={this.handleChange}
+        />
+        <input
+          name="area"
+          type="text"
+          placeholder="area"
+          onChange={this.handleChange}
+        />
+        <button
+          onClick={this.handleSubmit}
+        >Add
+        </button>
+      </>
+    );
+  }
+}
+
+export default AddListing;
